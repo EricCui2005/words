15056f292c83b8b20ae0cf97e192c5a1cddf8711
ericcui <ericcui2005@gmail.com>
2024-11-06 19:41:01 -0800

    Word transitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+'use client'
+
+import { useState, useEffect } from "react"
+
 export default function Home() {
+
+  // Tracking the status of the word
+  const [movedUp, setMovedUp] = useState(false)
+
+  useEffect(() => {
+
+    // Word transition logic
+    const wordTransition = () => {
+      setMovedUp(!movedUp);
+    }
+
+    // Adding an event listener to detect a click anywhere on the document
+    document.addEventListener('click', wordTransition)
+
+    // Cleanup function
+    return () => {
+      document.removeEventListener('click', wordTransition)
+    };
+  }, [movedUp])
+
   return (
     <>
       <div className="h-screen flex flex-col items-center justify-center">
-        <div>
-            <p>Word of the day</p>
-            <p>Word</p>
-            <p>Definition</p>
+        <div className="flex flex-col items-center justify-center gap-8">
+            <div className={`text-white text-7xl font-bold italic ${movedUp ? '-translate-y-6 duration-500' : 'translate-y-0 duration-500'}`}>Test</div>
+            <p className="text-white text-4xl">
+              1. Emitting light <br></br>
+              2. Emitting light 
+            </p>
         </div>
       </div>
     </>
