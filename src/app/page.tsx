5c0f11a346a80094e8c90ce0cd3d387acb645c9a
ericcui <ericcui2005@gmail.com>
2024-11-15 00:13:03 -0800

    Weird input field testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Word from "@/components/word"
 import Definition from "@/components/definition"
 import fetchWordData from "@/utility/wordFetch"
 import { WordData } from "@/utility/types"
+import { TextField } from "@mui/material"
 
 export default function Home() {
 
@@ -33,29 +34,40 @@ export default function Home() {
   }, [])
 
   // Adds a new listener each time with new word wordMovedUp state
-  useEffect(() => {
+  // useEffect(() => {
 
-    // Word transition logic
-    const wordTransition = () => {
-      setMovedUp(!wordMovedUp);
-    }
+  //   // Word transition logic
+  //   const wordTransition = () => {
+  //     setMovedUp(!wordMovedUp);
+  //   }
 
-    // Adding an event listener to detect a click anywhere on the document
-    document.addEventListener('click', wordTransition)
+  //   // Adding an event listener to detect a click anywhere on the document
+  //   document.addEventListener('click', wordTransition)
 
-    // Cleanup function
-    return () => {
-      document.removeEventListener('click', wordTransition)
-    };
-  }, [wordMovedUp])
+  //   // Cleanup function
+  //   return () => {
+  //     document.removeEventListener('click', wordTransition)
+  //   };
+  // }, [wordMovedUp])
+
+  const handleClick = (target) => {
+    setMovedUp(!wordMovedUp)
+    console.log("Hello")
+  }
 
   return (
     <>
       <div className="h-screen flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-8">
+        <div onClick={handleClick} className="flex flex-col items-center justify-center gap-8">
             <Word word={wordData ? wordData[0]?.meta?.id : "Loading..."} moved={wordMovedUp}/>
             <Definition definitions={definitionData ? definitionData : []} wordMoved={wordMovedUp}/>
         </div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" sx={{
+    width: '300px',
+    marginTop: 2,          // converts to 16px (1 = 8px)
+    backgroundColor: '#fff',
+    borderRadius: 10,       // converts to 8px
+  }}/>
       </div>
     </>
   )
