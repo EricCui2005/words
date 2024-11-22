6b08b75e55d791706f079c8c5bf6ff99bc5913dc
ericcui <ericcui2005@gmail.com>
2024-11-21 16:25:11 -0800

    Deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { useState } from "react"
+import { SetStateAction, useState } from "react"
 import Hamburger from "hamburger-react"
 import Menu from "@/components/menu"
 import DefinitionBlock from "@/components/definitionBlock"
 import SynonymBlock from "@/components/synonymBlock"
+import Button from '@mui/material/Button';
 
 export default function Home() {
 
@@ -14,13 +15,18 @@ export default function Home() {
   const handleToggle = () => {
     setIsOpen(!isOpen)
   }
+  const handleClick = (e: any) => {
+    setBlockType(e.target.id)
+  }
   
   return (
     <>
       <div className="relative h-screen relative flex flex-col items-center justify-center">
           <Hamburger toggled={isOpen} onToggle={handleToggle} color="white" rounded></Hamburger>
           <Menu isOpen={isOpen} className={"absolute"}>
-            <button>hi</button>
+            <Button variant="outlined" sx={{ width: 300, color: 'white' }} id="def" onClick={handleClick}>Definition</Button>
+            <Button variant="outlined" sx={{ width: 300, color: 'white' }} id="syn" onClick={handleClick}>Synonym</Button>
+            <Button variant="outlined" sx={{ width: 300, color: 'white' }} id="ant" onClick={handleClick}>Antonym</Button>
           </Menu>
           <div className={`absolute h-1/2 h-screen relative flex flex-col items-center justify-center transition-opacity duration-100 ${isOpen ? "opacity-0 pointer-events-none" : "opacity-100"}`}>
             {blockType == "def" && <DefinitionBlock/>}
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,9 +3,8 @@ export default function Menu({ children, isOpen, className }) {
     // Menu component
     return (
         <>
-            <div className={`menu-component ${className} absolute z-10 flex flex-col justify-center items-center rounded-3xl text-white w-96 h-96 bg-blue-700 transition-opacity 
+            <div className={`flex flex-col gap-8 menu-component ${className} absolute z-10 flex flex-col justify-center items-center rounded-xl text-white w-96 h-64 bg-blue-700 transition-opacity 
                 duration-500 ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}>
-                    <h1 className="mb-8 text-2xl">Words</h1>
                     {children}
             </div>
         </>
